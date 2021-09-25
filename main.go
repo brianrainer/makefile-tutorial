09b414d5b9c78436ab865c2560f0dcd0a383eb0b
Brian Rainer Suryaputra <brian.rainer@gudangada.com>
2021-09-25 09:47:09 +0700

    Add calculator

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/brianrainer/makefile-tutorial/calculator"
+)
 
 func main(){
 	fmt.Println("Hello!")
+	fmt.Println(calculator.Add(5,3))
 }
